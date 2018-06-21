ca80df4f738c744bec4f00af0fa568b6811d5a6b
elijah johnston <eli@millinno.com>
2018-06-21 07:32:10 +0200

    bot is up and running

--- app/bot/consultation_bot.rb
+++ app/bot/consultation_bot.rb
@@ -2,5 +2,23 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
-end
+  message.typing_on
+
+  message.reply(
+  text: Question.find(1).content ,
+  quick_replies: [
+    {
+      content_type: 'text',
+      title: 'Yes',
+      payload: 'HARMLESS'
+    },
+     {
+      content_type: 'text',
+      title: 'No',
+      payload: 'HARMLESS'
+    }
+  ]
+)
+
+
+
