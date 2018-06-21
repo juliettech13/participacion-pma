fff564c75d22e5a0af751bf08e5a163ba621bedf
Juliette Chevalier <juliettech.1393@gmail.com>
2018-06-21 16:48:43 -0500

    changed routes for legislation and consutlation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :consultations, only: [:show, :new, :create, :destroy] do
-    resources :legislations, only: [:show] do
-      resources :sections, only: [:show]
-    end
+  resources :legislations, only: [:show] do
+    resources :sections, only: [:show]
+    resources :consultations, only: [:show, :new, :create, :destroy]
   end
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
