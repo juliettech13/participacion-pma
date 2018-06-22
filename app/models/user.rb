3b1ace1a24924980cf80a042bebfcf8002046716
elijah johnston <eli@millinno.com>
2018-06-22 20:10:32 +0200

    added twitter and linkedin for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable, omniauth_providers: [:facebook, :twitter, :linkedin]
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
