6ad08fc8d892bcd13c1980793e0fe229796d7a42
elijah johnston <eli@millinno.com>
2018-06-25 21:26:04 +0200

    going with jq form_with

--- app/javascript/controllers/legislation_controller.js
+++ app/javascript/controllers/legislation_controller.js
@@ -11,10 +11,13 @@ export default class extends Controller {
     var selectSection = document.getElementById(selectChoice);
     var activeSection = document.getElementById('active_section')
     // var section = document.getElementsByClassName()
+    activeSection.innerHTML = '';
     activeSection.innerHTML = selectSection.innerHTML
     selectSection.style.display = "block";
     console.log("grab it", selectSection)
   };
+
+
 
   toggleQuestion() {
     console.log("grabbbbbb it!", this.element)
