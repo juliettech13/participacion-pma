5b646b180548ba2d4d052fdae392072be3d31563
elijah johnston <eli@millinno.com>
2018-06-25 17:48:20 +0200

    only section is showing

--- app/javascript/controllers/legislation_controller.js
+++ app/javascript/controllers/legislation_controller.js
@@ -5,6 +5,17 @@ export default class extends Controller {
     console.log("Hello, Stimulus!", this.element)
   }
 
+  showSection(e) {
+    var consultationBox = document.getElementById('document');
+    var selectChoice = e.target.innerHTML;
+    var selectSection = document.getElementById(selectChoice);
+    var activeSection = document.getElementById('active_section')
+    // var section = document.getElementsByClassName()
+    activeSection.innerHTML = selectSection.innerHTML
+    selectSection.style.display = "block";
+    console.log("grab it", selectSection)
+  };
+
   toggleQuestion() {
     console.log("grabbbbbb it!", this.element)
 
