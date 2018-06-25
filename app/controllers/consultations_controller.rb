0a98c8f04fbd2bdbe4c7b64f21c51a95aeece1dd
Juliette Chevalier <juliettech.1393@gmail.com>
2018-06-25 15:12:30 -0500

    cleaning up commment section

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -3,30 +3,17 @@ class ConsultationsController < ApplicationController
   before_action :set_consultation, only: [:show, :destroy]
   before_action :authenticate_user!, only: [:create]
 
-
-  # def index
-  #   @consultations = Consultation.all
-  # end
-
   def show
   end
 
   def new
-    @legislation = Legislation.find(1)
+    @legislation = Legislation.find(params[:legislation_id])
     @consultation = Consultation.new
-    #   respond_to do |format|
-    #     format.html  # new.html.erb
-    #     format.json  { render :json => @user }
-    # end
   end
-
-  # def edit
-  # end
 
   def create
     # should redirect to the start of the consultation, which is the legislation/show.
     @consultation = Consultation.new(consultation_params)
-    # @legislation = legislation.find(params[:legislation_id])
     @legislation = Legislation.find(params[:legislation_id])
 
     if current_user == nil
@@ -37,30 +24,7 @@ class ConsultationsController < ApplicationController
     end
     @consultation.save!
     redirect_to legislation_path(@legislation)
-
-    # respond_to do |format|
-    #   if @consultation.save
-    #     flash[:success] = "Thanks! I'll be in touch soon!"
-    #     format.html { redirect_to @consultation, notice: 'Consultation was successfully created.' }
-    #     format.json { render :show, status: :created, location: @consultation }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @consultation.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
-
-  # def update
-  #   respond_to do |format|
-  #     if @consultation.update(consultation_params)
-  #       format.html { redirect_to @consultation, notice: 'Consultation was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @consultation }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @consultation.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   def destroy
     @consultation.destroy
