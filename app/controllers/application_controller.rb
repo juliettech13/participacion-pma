fed3cca5df3d7c1e311f51878701430331bf7a06
Nathan Lebas <nathanlebas3@gmail.com>
2018-06-26 18:22:16 +0200

    work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ class User < ApplicationRecord
     return user
   end
 
+  # <--- Twitter --->
   def self.find_or_create_from_auth_hash(auth_hash)
     user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
     user.update(
