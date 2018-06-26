34588fe0061e72cdf850a4144f907bf0141662f1
Juliette Chevalier <juliettech.1393@gmail.com>
2018-06-26 08:00:19 -0500

    cleaned answers controller from innecessary comments

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,28 +1,20 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
-  # GET /answers
-  # GET /answers.json
   def index
     @answers = Answer.all
   end
 
-  # GET /answers/1
-  # GET /answers/1.json
   def show
   end
 
-  # GET /answers/new
   def new
     @answer = Answer.new
   end
 
-  # GET /answers/1/edit
   def edit
   end
 
-  # POST /answers
-  # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
     @answer.question = @question
@@ -38,8 +30,6 @@ class AnswersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
   def update
     respond_to do |format|
       if @answer.update(answer_params)
@@ -52,8 +42,6 @@ class AnswersController < ApplicationController
     end
   end
 
-  # DELETE /answers/1
-  # DELETE /answers/1.json
   def destroy
     @answer.destroy
     respond_to do |format|
