6e34e0b2d1b4b55e037983cc9ce71812aec5a633
elijah johnston <eli@millinno.com>
2018-06-27 16:14:19 +0200

    all coming together

--- app/javascript/controllers/instruction_controller.js
+++ app/javascript/controllers/instruction_controller.js
@@ -16,7 +16,7 @@ export default class extends Controller {
   toggleForward(){
     console.log('little engine')
     var activeInstruction = document.getElementById('instructions');
-    var firstInstruction = "The vision intends to create a unifying policy vision for transforming [COUNTRY] economy and accelerating economic growth. The vision is composed of four chapters, and you are invited to comment on specific policy recommendations, on chapters, on the introduction and on the entire policy.";
+    var firstInstruction = "The vision intends to create a unifying policy vision for transforming Nigeria's economy and accelerating economic growth. The vision is composed of four chapters, and you are invited to comment on specific policy recommendations, on chapters, on the introduction and on the entire policy.";
     var middleInstruction = "Leave and come back anytime, your progress is saved.";
     var lastInstruction = "Click on any of the sections or clauses. If you like something, please let us know. If you think we can improve anything to support innovation, entrepreneurship or ICT developmemt, please let us know how. Thanks for your participation!";
     var next = document.getElementById("nextbutton");
