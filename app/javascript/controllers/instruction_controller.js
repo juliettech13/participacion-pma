6eb465eaba8c1d18cb527b791a3a22328f2a6ac6
elijah johnston <eli@millinno.com>
2018-06-27 19:07:58 +0200

    added a bit more metadata

--- app/javascript/controllers/instruction_controller.js
+++ app/javascript/controllers/instruction_controller.js
@@ -17,8 +17,9 @@ export default class extends Controller {
     console.log('little engine')
     var activeInstruction = document.getElementById('instructions');
     var firstInstruction = "The vision intends to create a unifying policy vision for transforming Nigeria's economy and accelerating economic growth. The vision is composed of four chapters, and you are invited to comment on specific policy recommendations, on chapters, on the introduction and on the entire policy.";
-    var middleInstruction = "Leave and come back anytime, your progress is saved.";
-    var lastInstruction = "Click on any of the sections or clauses. If you like something, please let us know. If you think we can improve anything to support innovation, entrepreneurship or ICT developmemt, please let us know how. Thanks for your participation!";
+    var secondInstruction = "Navigate through the policy and <strong>click on clauses</strong> to interact with them. You will be presented with questions using a 1 through 5 scale, 1 means <strong>you strongly oppose</strong> and 5 means <strong>you are strongly in favor</strong>.";
+    var thirdInstruction = "After answering the first two questions, you will have the opportunity to <strong>suggest a revision</strong> should you feel compelled. There is no maximum or minimum participation, go at your own pace.";
+    var fourthInstruction = "If you like something, please let us know. If you think we can improve anything to support innovation, entrepreneurship or ICT developmemt, please let us know how. Thanks for your participation!";
     var next = document.getElementById("nextbutton");
     var back = document.getElementById("backbutton");
 
@@ -27,16 +28,17 @@ export default class extends Controller {
       switch (activeInstruction.innerHTML)
       {
         case firstInstruction:
-          activeInstruction.innerHTML = middleInstruction;
+          activeInstruction.innerHTML = secondInstruction;
           back.style.display = 'none' ? '' : 'none';
           break;
-        case middleInstruction:
-          activeInstruction.innerHTML = lastInstruction;
+        case secondInstruction:
+          activeInstruction.innerHTML = thirdInstruction;
+          break;
+        case thirdInstruction:
+          activeInstruction.innerHTML = fourthInstruction;
           document.getElementById("start").style.display = 'none' ? '' : 'none';
           next.style.display = 'none';
           break;
-        default:
-          alert('Default case');
       }
 
   }
@@ -44,26 +46,28 @@ export default class extends Controller {
   toggleBack(){
     console.log('little engine')
     var activeInstruction = document.getElementById('instructions');
-    var firstInstruction = "The vision intends to create a unifying policy vision for transforming [COUNTRY] economy and accelerating economic growth. The vision is composed of four chapters, and you are invited to comment on specific policy recommendations, on chapters, on the introduction and on the entire policy.";
-    var middleInstruction = "Leave and come back anytime, your progress is saved.";
-    var lastInstruction = "Click on any of the sections or clauses. If you like something, please let us know. If you think we can improve anything to support innovation, entrepreneurship or ICT developmemt, please let us know how. Thanks for your participation!";
+    var firstInstruction = "The vision intends to create a unifying policy vision for transforming Nigeria's economy and accelerating economic growth. The vision is composed of four chapters, and you are invited to comment on specific policy recommendations, on chapters, on the introduction and on the entire policy.";
+    var secondInstruction = "Navigate through the policy and <strong>click on clauses</strong> to interact with them. You will be presented with questions using a 1 through 5 scale, 1 means <strong>you strongly oppose</strong> and 5 means <strong>you are strongly in favor</strong>.";
+    var thirdInstruction = "After answering the first two questions, you will have the opportunity to <strong>suggest a revision</strong> should you feel compelled. There is no maximum or minimum participation, go at your own pace.";
+    var fourthInstruction = "If you like something, please let us know. If you think we can improve anything to support innovation, entrepreneurship or ICT developmemt, please let us know how. Thanks for your participation!";
     var back = document.getElementById("backbutton");
     var next = document.getElementById("nextbutton");
 
 
      switch (activeInstruction.innerHTML)
       {
-        case middleInstruction:
+        case secondInstruction:
           activeInstruction.innerHTML = firstInstruction;
           back.style.display = 'none';
           break;
-        case lastInstruction:
-          activeInstruction.innerHTML = middleInstruction;
+        case thirdInstruction:
+          activeInstruction.innerHTML = secondInstruction;
+          break;
+        case fourthInstruction:
+          activeInstruction.innerHTML = thirdInstruction;
           document.getElementById("start").style.display = 'none';
           next.style.display = 'none' ? '' : 'none';
           break;
-        default:
-          alert('Default case');
       }
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -83,7 +83,8 @@ Clause.create!(content: 'Mandate infrastructure sharing to achieve the economies
   Metadata.create!(
     agency: 'Nigerian Communications Commission (NCC)',
     rationale: 'This would bring down the cost and ensure affordable access.'
-    status: 'Status: NCC published guidelines on collocation and infrastructure sharing on Rights of Way, Masts, Poles, Antenna and tower-structures, Ducts, Trenches, Electric power. In the guidelines, each operator (licensee) is expected to publish standard price list, this is not regulated by NCC which could be exploited by the operators. NCC maintains a list of operators on its website, this includes the name of the company, address, start and expiry dates. This list may not be conclusive as there are operators that are not listed there but in previous published lists. The issue with the guidelines is that it is only for passive sharing, there is no guidelines on active sharing. However, NCC is working on a framework on active sharing but this is yet to be published. The Nigerian National Broadband Plan 2013 - 2018 is aimed at  “connecting communities with high speed internet and broadband access to facilitate faster socioeconomic advancement of the nation and its people”. To achieve this, the government, through NCC require Infrastructure Companies (Infracos) to bid for license to provide broadband infrastructure on an open access, non-discriminatory, price regulated basis to the six geopolitical zones and Lagos. Licenses have been awarded for Lagos, North Central, South East, North East. The license for North West, South South and South West have purportedly been awarded, The network and services infrastructure is divided into 3 layers; Passive Infrastructure Layer, Wholesale layer, Retail Service Providers (RSP). The main challenge telecommunication companies face is security, to this end, NCC and Nigerian Security and Civil Defence Corps (NSCDC) signed an MOU to protect the infrastructure. This would go along way in alleviating this challenge which would in turn increase access to affordable broadband. Furthermore, the government plans to lay 18,000KM of fibre in the country in its effort to achieve and surpass the 30% broadband penetration target of 2018.'
+    status: 'Status: NCC published guidelines on collocation and infrastructure sharing on Rights of Way, Masts, Poles, Antenna and tower-structures, Ducts, Trenches, Electric power. In the guidelines, each operator (licensee) is expected to publish standard price list, this is not regulated by NCC which could be exploited by the operators. NCC maintains a list of operators on its website, this includes the name of the company, address, start and expiry dates. This list may not be conclusive as there are operators that are not listed there but in previous published lists. The issue with the guidelines is that it is only for passive sharing, there is no guidelines on active sharing. However, NCC is working on a framework on active sharing but this is yet to be published. The Nigerian National Broadband Plan 2013 - 2018 is aimed at  “connecting communities with high speed internet and broadband access to facilitate faster socioeconomic advancement of the nation and its people”. To achieve this, the government, through NCC require Infrastructure Companies (Infracos) to bid for license to provide broadband infrastructure on an open access, non-discriminatory, price regulated basis to the six geopolitical zones and Lagos. Licenses have been awarded for Lagos, North Central, South East, North East. The license for North West, South South and South West have purportedly been awarded, The network and services infrastructure is divided into 3 layers; Passive Infrastructure Layer, Wholesale layer, Retail Service Providers (RSP). The main challenge telecommunication companies face is security, to this end, NCC and Nigerian Security and Civil Defence Corps (NSCDC) signed an MOU to protect the infrastructure. This would go along way in alleviating this challenge which would in turn increase access to affordable broadband. Furthermore, the government plans to lay 18,000KM of fibre in the country in its effort to achieve and surpass the 30% broadband penetration target of 2018.',
+    clause_id: 2
     )
 
 Clause.create!(content: 'Drop Right of Way charges for fiber distribution, duties on telecommunication imports and the Information Technology tax to reduce capital requirements for expanding digital infrastructure, thereby reducing the costs passed on to consumers.', section_id: 2)
@@ -96,7 +97,8 @@ Clause.create!(content: 'Drop Right of Way charges for fiber distribution, dutie
     agency: 'Federal Inland Revenue Services (FIRS), Federal Ministry of Power, Works and Housing, State Inland Revenue Board, Local Government Authority, Nigerian Communications Commission (NCC), Nigeria Customs Service (NCS), Federal Ministry of Finance (how about the respective state agencies)',
     rationale: 'Lack of harmonised RoW across federal, state and LGA and multiple taxation increases the cost of service delivery which is passed on to the consumers. Elimination of such charges, duties and tax would encourage investments and reduce the cost of service delivery.'
     status: "Currently, there is a guideline for the grant and access of right way to ICT services on federal roads by the Federal Ministry of Power, Works and Housing. The charges are N145 per linear meter for ducts and cables and N20 per linear metre annual maintenance fees subject to change every 5 years. States and Local Governments have different charges and this varies from state to state. The lack of uniform charges is one of the challenges telcos face which affects the supply of services. To address this challenge, NEC approved a policy on harmonization of right of way charges across LG, states and federal highways. The NEC RoW guideline still maintains the amount of N145 per meter for laying fibre for all parts of the country. Also, NCC made a case to the Governors Forum to ensure on the need to adhere to the NEC’s resolutions. Right of Way is included in the Nigerian Communications Commission’s Guidelines on Collocation and Infrastructure Sharing, this provides a means of bringing down the cost of broadband as shown in Vision 1.
-The import duties on all telecommunications equipment is 10% with the exception of parts and accessories of instruments/apparatus, designed for telecommunications which is 5%. In the NITDA establishment Act, all telecommunications companies, GSM Service Providers, Cyber Companies and Internet Providers with annual turnover of 100M and above are liable to pay 1%  of the profit before tax."
+    The import duties on all telecommunications equipment is 10% with the exception of parts and accessories of instruments/apparatus, designed for telecommunications which is 5%. In the NITDA establishment Act, all telecommunications companies, GSM Service Providers, Cyber Companies and Internet Providers with annual turnover of 100M and above are liable to pay 1%  of the profit before tax.",
+    clause_id: 3
     )
 
 Clause.create!(content: 'Provide telecommunications companies with prioritised access to foreign exchange windows, such as the  Secondary Market Intervention Scheme (SMIS).', section_id: 2)
@@ -105,11 +107,25 @@ Clause.create!(content: 'Provide telecommunications companies with prioritised a
   Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 4)
   Question.create!(content: 'Please suggest your revision', clause_id: 4)
 
+  Metadata.create!(
+    agency: "Central Bank of Nigeria (CBN)",
+    rationale: "Access to SMIS will boost the investment in the telecommunications sector and accelerate broadband penetration in Nigeria.",
+    status: "All FX transactions are fall under the Investors’ and Exporters’ (I & E) FX Window which was established by the CBN, and another Window for Secondary Market Intervention Sales (SMIS), wholesale and retail. CBN at its discretion chooses the sub-sectors of the economy to access the SMIS. Access to subsidised foreign exchange will attract investment in the sector which in turn will accelerate broadband penetration.",
+    clause_id: 4
+  )
+
 Clause.create!(content: 'Adopt a “dig once” policy, so that any traditional infrastructure investment--roads, electricity, water, etc--must be accompanied by fibre optic cables, where none already exist.', section_id: 2)
   Question.create!(content: 'How well does this represents your views?', clause_id: 5)
   Question.create!(content: 'Does this achieve ICT greatness for the country?', clause_id: 5)
   Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 5)
   Question.create!(content: 'Please suggest your revision', clause_id: 5)
+
+   Metadata.create!(
+    agency: "Ministry of Power, Works and Housing, Ministry of Environment",
+    rationale: " Implementing “dig once” policy will ensure the protection of the environment, increase broadband penetration and affordable access.",
+    status: "Anambra State included the implementation of Dig Once policy as part of its smart city initiative.",
+    clause_id: 5
+  )
 
 Clause.create!(content: 'Launch a single e-Governance portal using a Whole of Government approach to rationalize and simplify access to government services.', section_id: 2)
   Question.create!(content: 'How well does this represents your views?', clause_id: 6)
@@ -117,6 +133,12 @@ Clause.create!(content: 'Launch a single e-Governance portal using a Whole of Go
   Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 6)
   Question.create!(content: 'Please suggest your revision', clause_id: 6)
 
+ Metadata.create!(
+    agency: "National Information Technology Development Agency (NITDA), National eGovernment Strategies (NeGSt), MDAs",
+    rationale: "Simplified access to government services will improve transparency of common processes, data integrity and reduce the time and cost associated with accessing information.",
+    status: "NeGSt was created to “facilitate, drive and implement” eGovernment programmes using a Public Private Partnership (PPP) model. They provide MDAs with support in terms of deployment of eGovernment services and solutions. NITDA recently published the Nigerian e-Government Interoperability Framework (Ne-GIF) which adopts a  Whole-of-Government approach which will lead to Government Digital Transformation. The purpose of this framework is to ease data collection and sharing among MDAs and promote interactions between government, businesses and citizens.",
+    clause_id: 6
+  )
 
 Clause.create!(content: 'Build a national data infrastructure to enable innovation:
 a.  Ensure that an Open Data staff/unit responsible for opening up agency data and responding to Freedom of Information requests is operational within every MDA,
@@ -129,6 +151,24 @@ e.  Launch a fund to procure API development from the indigenous technology comm
   Question.create!(content: 'Does this achieve ICT greatness for the country?', clause_id: 7)
   Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 7)
   Question.create!(content: 'Please suggest your revision', clause_id: 7)
+
+  Metadata.create!(
+    agency: "National Information Technology Development Agency (NITDA), Federal Ministry of Justice, National Bureau of Statistics (NBS), and all MDAs",
+    rationale: "Data is vital for any economy to progress, its availability to policy makers, businesses and citizens has direct impact on how government delivers services.",
+    status: "The enactment of the FOI Act in 2011 has made public records and information more freely available and accessible to Nigerians. Several government agencies have set up compliance structures that now enable them to respond to requests for information within the ambit of the law. However, several issues such as:
+Low awareness, skills and knowledge of the FOI Act provisions and processes by citizens and public/civil servants.
+Lack of designated officers responsible for Freedom of Information in most public Institutions.
+Lack of sanctions against public institutions and FOI responsible officers who do not respond to FOI requests or/and report to FMoJ annually.
+Lack of Infrastructure to support digital collection and management of information, to facilitate timely retrieval of requested information.
+This policy vision seeks to expand upon best-practice, ensure compliance and
+Ensure that an Open Data staff/unit responsible for opening up agency data and responding to Freedom of Information requests is operational within every MDA,
+Amend the Freedom of Information Act to strengthen enforcement and compliance,
+Build a single digital platform to electronically process FOI requests for every MDA,
+The Bureau of Public Service Reform (BPSR) has adopted a unique electronic FOI platform on its website that gives real-time information to citizens and also encourages voluntary disclosure. This policy seeks to expand this best-practice and provide a single electronic portal for FOI requests that can be forwarded to each MDA and tracked. This will not only increase awareness
+Build a single national data platform that aligns access to government datasets
+Launch a fund to procure API development from the indigenous technology community.",
+    clause_id: 7
+    )
 
 Clause.create!(content: 'Support renewable energy through expanded subsidies and tax exemptions for importation of renewable energy technology.', section_id: 2)
   Question.create!(content: 'How well does this represents your views?', clause_id: 8)
@@ -277,35 +317,7 @@ section_id: 5)
 
 
 puts "we done!"
-# Question.create!(content: 'Do you feel that this represents your views?', clause_id: 1)
-# Question.create!(content: 'How well does this push your country in the direction?', clause_id: 1)
-# Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 1)
-# Question.create!(content: 'Please suggest a revision', clause_id: 1)
 
-# Question.create!(content: 'Do you feel that this represents your views?', clause_id: 2)
-# Question.create!(content: 'How well does this push your country in the direction?', clause_id: 2)
-# Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 2)
-# Question.create!(content: 'Please suggest a revision', clause_id: 2)
-
-# Question.create!(content: 'Do you feel that this represents your views?', clause_id: 3)
-# Question.create!(content: 'How well does this push your country in the direction?', clause_id: 3)
-# Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 3)
-# Question.create!(content: 'Please suggest a revision', clause_id: 3)
-
-# Question.create!(content: 'Do you feel that this represents your views?', clause_id: 4)
-# Question.create!(content: 'How well does this push your country in the direction?', clause_id: 4)
-# Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 4)
-# Question.create!(content: 'Please suggest a revision', clause_id: 4)
-
-# Question.create!(content: 'Do you feel that this represents your views?', clause_id: 5)
-# Question.create!(content: 'How well does this push your country in the direction?', clause_id: 5)
-# Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 5)
-# Question.create!(content: 'Please suggest a revision', clause_id: 5)
-
-# Question.create!(content: 'Do you feel that this represents your views?', clause_id: 6)
-# Question.create!(content: 'How well does this push your country in the direction?', clause_id: 6)
-# Question.create!(content: 'Would you like to provide a suggestion?', clause_id: 6)
-# Question.create!(content: 'Please suggest a revision', clause_id: 6)
 
 puts "creating dummy general feedback"
 GeneralFeedback.create!(content: 'My custom feedback 1', legislation_id: 1)
