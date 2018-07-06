f6d0df30447a98aa9775ba3dcee37af7bc341b8c
Nathan Lebas <nathanlebas3@gmail.com>
2018-07-06 13:40:13 +0200

    the facebook login is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
--- config/routes.rb
+++ config/routes.rb
@@ -29,8 +29,8 @@ Rails.application.routes.draw do
   resources :answers
 
 
-  devise_for :users
-    # controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
