7eaf7bb85654984a45dd94e091e6e900fb3a06e7
elijah johnston <eli@millinno.com>
2018-07-09 16:58:13 -0400

    removed extra from devise.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,26 +28,4 @@ class User < ApplicationRecord
 
     return user
   end
-
-def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-  user = User.where(:provider => auth.provider, :uid => auth.uid).first
-  unless user
-    user = User.create(name:auth.extra.raw_info.name,
-                         provider:auth.provider,
-                         uid:auth.uid,
-                         email:auth.info.email,
-                         password:Devise.friendly_token[0,20]
-                         )
-  end
-  user
-end
-
-def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
-        user.email = data["email"]
-      end
-    end
-  end
-
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,18 +3,24 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
  Devise.setup do |config|
-  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], token_params: { parse: :json },
-     scope: 'email',
-     info_fields: 'email, first_name, last_name',
-     image_size: 'square',
-     secure_image_url: true,
-     client_options: {
-       site: "https://graph.facebook.com/v2.3",
-       authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
-     },
-     token_params: {
-       parse: :json
-     }
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+    scope: 'email',
+    info_fields: 'email, first_name, last_name',
+    image_size: 'square',  # 50x50, guaranteed ratio
+    secure_image_url: true
+
+  # config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"],
+  #    scope: 'email',
+  #    info_fields: 'email, first_name, last_name',
+  #    image_size: 'square',
+  #    secure_image_url: true,
+  #    client_options: {
+  #      site: "https://graph.facebook.com/v2.3",
+  #      authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
+  #    },
+  #    token_params: {
+  #      parse: :json
+  #    }
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
