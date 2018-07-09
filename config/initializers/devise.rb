47ad836b0ded002404de5dc081f688ed675d5d0a
elijah johnston <eli@millinno.com>
2018-07-09 15:48:00 -0400

    removed some fields

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,13 +8,13 @@
      info_fields: 'email, first_name, last_name',
      image_size: 'square',
      secure_image_url: true,
-     client_options: {
-       site: "https://graph.facebook.com/v2.3",
-       authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
-     },
-     token_params: {
-       parse: :json
-     }
+     # client_options: {
+     #   site: "https://graph.facebook.com/v2.3",
+     #   authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
+     # },
+     # token_params: {
+     #   parse: :json
+     # }
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
