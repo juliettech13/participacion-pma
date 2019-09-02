7dca5e33658ae8197c31b7bd4e36b4622c55cda1
Juliettech <juliettech.1393@gmail.com>
2019-09-02 17:56:11 -0500

    added migration to change column type of number to float to be able to display order of articles accordingly. also added  method in article model to be able to turn the number to integer for display. then adapted seeds to this new reality and the expected order of each article with its appropriate subarticles.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_001535) do
+ActiveRecord::Schema.define(version: 2019_09_02_215751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_001535) do
 
   create_table "articles", force: :cascade do |t|
     t.text "content"
-    t.integer "number"
+    t.float "number"
     t.bigint "chapter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
