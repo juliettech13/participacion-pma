f1463716b85e0527a1f03615d762d8930319f7e7
Juliettech <juliettech.1393@gmail.com>
2019-09-06 12:01:02 -0500

    changed name and size of #answered_badge. added a fix height and overflow scroll to allow for scrolling inside metadatum. trying to fix the a/button problem for MODIFICADO badge in legislation show.

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -16,12 +16,12 @@ export default class extends Controller {
 
   answered(){
     var answerSection = this.element.parentNode.parentNode;
-    answerSection.insertAdjacentHTML('beforeend', '<span class="badge badge-success badges p-2" id="answered_badge">Respondido</span>');
+    answerSection.insertAdjacentHTML('beforeend', '<span class="badge badge-success badges p-2" id="answered_badge">Resultado Recibido</span>');
   }
 
   revised(){
     var answerSection = this.element.parentNode.parentNode;
-    answerSection.insertAdjacentHTML('beforeend', '<span class="badge badge-success badges p-2" id="revised_badge">Sugerido</span>');
+    answerSection.insertAdjacentHTML('beforeend', '<span class="badge badge-success badges p-2" id="revised_badge">Sugerencia Recibida</span>');
   }
 
   scrolltop(){
