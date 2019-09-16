cc756958bf6b0760958e336f8bbb7928725d177f
Juliettech <juliettech.1393@gmail.com>
2019-09-16 17:10:48 -0500

    added index and show views to most tabs in admin page

--- app/admin/general_feedbacks.rb
+++ app/admin/general_feedbacks.rb
@@ -14,4 +14,38 @@ ActiveAdmin.register GeneralFeedback do
   config.clear_action_items!
   actions :all, except: [:edit, :update, :destroy]
   menu priority: 2
+
+  index do
+    column "Nombre" do |general_feedback|
+      user = general_feedback.consultation.user
+      link_to (user.first_name ? user.first_name : "No dio nombre al registrarse"), admin_user_path(user)
+    end
+
+    column "Feedback" do |general_feedback|
+      general_feedback.content
+    end
+
+     column "Fecha" do |general_feedback|
+      general_feedback.created_at.strftime("%d/%m/%y")
+    end
+  end
+
+  show do
+    panel "Usuarios" do
+      table_for user do
+        column "Nombre" do |user|
+          user.first_name ? user.first_name : "No dio nombre al registrarse"
+        end
+
+        column "Apellido" do |user|
+          user.last_name ? user.last_name : "No dio nombre al registrarse"
+        end
+
+        column "Email" do |user|
+          user.email
+        end
+      end
+    end
+    active_admin_comments
+  end
 end
