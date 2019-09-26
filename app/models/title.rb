20b6ed7df5a397f861fe95eb14e5318a683df558
Juliettech <juliettech.1393@gmail.com>
2019-09-26 19:20:33 +0100

    changed layout of home page and font for better styling

--- app/models/title.rb
+++ app/models/title.rb
@@ -13,18 +13,3 @@ class Title < ApplicationRecord
     self.description = new_title
   end
 end
-
-
-# <div class="header-article">
-#   <div class="d-flex flex-row">
-#   <% if article.new == true %>
-#     <h6 class="align-left p-2">Artículo</h6>
-#     <div class="p-2 badge badge-warning">Nuevo</div>
-#   <% else %>
-#     <h6 class="align-left p-2">Artículo <%= article.number %></h6>
-#     <% if Metadatum.exists?(article: article) %>
-#       <% @metadata = Metadatum.find_by(article: article) %>
-#       <a data-html="true" data-toggle="popover" data-trigger="hover" title="Artículo Modificado" data-content="<%= render 'metadata_show', metadata: @metadata %>" class="p-2 badge badge-warning">Modificación</a>
-#     <% end %>
-#   <% end %>
-#  </div>
