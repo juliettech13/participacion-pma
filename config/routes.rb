899a45c1e30be21fcadfea5c8f028004e4442775
Juliettech <juliettech.1393@gmail.com>
2019-09-30 08:53:45 +0100

    added design for home page and corrected copy

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,15 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  # root to: 'devise/registrations#new'
-
   get 'download_pdf', to: "legislations#download_pdf"
 
   resources :legislations, only: [:show] do
-    # resources :sections, only: [:show]
     resources :consultations, only: [:new, :create, :destroy]
   end
 
   resources :consultations, only: [:show] do
     resources :general_feedbacks, only: [:show, :new, :create]
   end
-
-  # resources :clauses, only: [:show] do
-  #   resources :questions, only: [:show]
-  # end
-
-  # resources :metadatum, only: [:show]
 
   get "/pages/finished", to: 'pages#finished'
 
