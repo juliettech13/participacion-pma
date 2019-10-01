73dad2bf4a257f9bffdb8e065f0efc9c9c0d67cf
Juliettech <juliettech.1393@gmail.com>
2019-10-01 01:54:56 +0100

    upgraded webpacker, added uglifier npm package, bettered design

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,19 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+require("uglifyjs-webpack-plugin")
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        include: /\/includes/,
+      }),
+    ],
+  },
+};
+
+// environment.plugins.delete("UglifyJs")
+// environment.plugins.append("UglifyJs", new UglifyJSPlugin())
+
 module.exports = environment.toWebpackConfig()
